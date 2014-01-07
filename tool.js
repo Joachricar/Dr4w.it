9d7e3c18249bd32510ec6a02f4913ae1de9e9c03
Joachim Carlsen <joachimcarlsen@gmail.com>
2014-01-07 12:50:01 +0100

    tool-template

--- tool.js
+++ tool.js
@@ -1,33 +1,47 @@
-function Pencil() {
+// Tool template
+function ToolType() {
 	var self = this;
 	
+	// tool attributes
+	// 	optional
 	self.width = 2;
-	self.name = "pencil";
-	self.description = "Just a pencil";
-	self.icon = "/images/icons/pencil.png";
+	//  required(used by drawit)
+	self.name = "a unique name";
+	self.description = "Crazy tool shit";
+	self.icon = "/images/icons/someIconForThisTool.png";
 	
+
 	self.buildMenu = function() {
 		var div = $("<div class='tool-settings'>");
-		$("<input type='number' id='pencil-width'>" + self.width + "</input>").change(function() {
+		$("<input type='number' id='tool-width'>" + self.width + "</input>").change(function() {
 			self.width = $(this).val();
 		}).appendTo(div);
 		return div;
 	}
 
+	// name: "draw", "up", "down", "enter", "leave"
 	self.inputEvent(name, e) {
-
+		
 	}
 
+	// draw stuff
 	self.draw(data) {
 
 	}
 
+	// set which canvas to draw to
 	self.setCanvas = function(c) {
 		self.canvas = c;
 	}
 }
 
-var tool = new Pencil();
+// temp variable
+// this is used when the tool is loaded
+// do not edit name
+var tool = new ToolType();
+
+// add to tool-list
 tools[tool.name] = tool;
 
+// opt msg
 console.log("PENCIL LOADED");
